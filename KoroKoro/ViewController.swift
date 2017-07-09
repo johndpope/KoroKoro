9b274b2219fbcff941d6bedefe5e8fdef60741a8
M.Ike <mike.app.info@gmail.com>
2017-07-09 22:54:31 +0900

    obj追加

--- KoroKoro/ViewController.swift
+++ KoroKoro/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet private var sceneView: ARSCNView!
+    
+    private var configuration: ARWorldTrackingSessionConfiguration {
+        let configuration = ARWorldTrackingSessionConfiguration()
+        return configuration
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        sceneView.showsStatistics = true
+        
+        let node = SCNNode(geometry: SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0))
+        node.position = SCNVector3(0, 0, -1)
+        sceneView.scene.rootNode.addChildNode(node)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
 }
 
